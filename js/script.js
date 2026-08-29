93359e2f017d11fffbaa942ae9dd12796472d8c4
Daniel M Santos <danielmelosantos5901@gmail.com>
2026-08-28 23:04:37 -0300

    Reatividade da aba perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -24,3 +24,25 @@ function ativarProduto(parametro) {
 }
 
 parametros.forEach(ativarProduto);
+
+// Perguntas frequentes (página seguro.html)
+
+const perguntas = document.querySelectorAll('.perguntas button');
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+
+  
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+  console.log(controls);
+}
+
+function eventosPergunta(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPergunta);
